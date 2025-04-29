1368bfcd502a05ef05218e8df08888a2cb5f40e9
mamatapoudel777 <131430891+mamatapoudel777@users.noreply.github.com>
2025-04-29 17:30:47 +0545

    first commit

---
+++ connection.php
@@ -0,0 +1,14 @@
+<?php
+$db_server = "localhost";
+$db_user = "root";
+$db_pass = "2607";
+$db_name = "teacherdb"; 
+
+$conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
+
+if ($conn) {
+    echo "Connection successful";
+} else {
+    echo "No connection";
+}
+?>
---
+++ declare.php
@@ -0,0 +1,8 @@
+<?php
+//variable declaration
+$x=10;
+$y=15.7;
+$name="mamata";
+echo $x+$y . "<br>";
+echo "Hi " . $name;
+?>
